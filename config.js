161164b83bdcb3ae070d81532b8427240a325f69
masakihataguchi-svg <masaki.hataguchi@gmail.com>
2026-05-05 12:17:51 +0700

    新規アルバム作成不可のデバッグ

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
  */
 const CONFIG = {
   // 新しくデプロイしたGASのWebアプリURLをここに貼り付けてください
-  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbwNiF9m2wALA9UtVf46zEaFRtvDsmtjJPP4FmwuMgVBLhTfUBbQQ-IfjqgXAdgxrmPEjw/exec'
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbwTYW6KWsOSDfVAbHaNA0j34V7voHw1A-7FKNPkyK22ceNnyOhhn2GW0iR1fMsE5NgnZw/exec'
 };
