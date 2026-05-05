fae1d66cbe1659bd35e004f1490892649563961f
masakihataguchi-svg <masaki.hataguchi@gmail.com>
2026-05-05 11:30:10 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+/**
+ * アプリケーション設定ファイル
+ */
+const CONFIG = {
+  // 新しくデプロイしたGASのWebアプリURLをここに貼り付けてください
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbwNiF9m2wALA9UtVf46zEaFRtvDsmtjJPP4FmwuMgVBLhTfUBbQQ-IfjqgXAdgxrmPEjw/exec'
+};
