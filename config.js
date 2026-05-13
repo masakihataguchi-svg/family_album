1233cbbca405e3d3e14ee5b924a8dff3674cd943
masakihataguchi-svg <masaki.hataguchi@gmail.com>
2026-05-13 17:00:48 +0700

    カバー画像指定機能追加

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
  */
 const CONFIG = {
   // 新しくデプロイしたGASのWebアプリURLをここに貼り付けてください
-  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbzHED3dgV1ZCOa_cdrJJIloLTt8mGl3xJuO5GZS7gQJmwExSE-w2HW6g8zWiSkP8vu7OA/exec'
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbxls6jS0HvldQBNYFbEyI2-NvBR8W7yEy8ZgCVgN6AOrLYiWdbnDUPJe4H_uDPT5yJL4Q/exec'
 };
