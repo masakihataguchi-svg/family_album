76da7e59b03c750b37e2dda4faabd75384edc546
masakihataguchi-svg <masaki.hataguchi@gmail.com>
2026-05-14 09:46:00 +0700

    カバー画像のマークを識別する機能の追加

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
  */
 const CONFIG = {
   // 新しくデプロイしたGASのWebアプリURLをここに貼り付けてください
-  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbxls6jS0HvldQBNYFbEyI2-NvBR8W7yEy8ZgCVgN6AOrLYiWdbnDUPJe4H_uDPT5yJL4Q/exec'
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbz0HNy8HJq5Ky1CFfCroUkADFtoGz6WyafB167RT8qUaiOfRFdmVQsjxdGr06Jni78w/exec'
 };
