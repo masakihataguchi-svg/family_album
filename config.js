8b79a2e1f0dd4fb34c7da0d749f279859fd3c490
masakihataguchi-svg <masaki.hataguchi@gmail.com>
2026-05-17 15:11:41 +0700

    エラーログローカル保存

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
  */
 const CONFIG = {
   // 新しくデプロイしたGASのWebアプリURLをここに貼り付けてください
-  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbzgv0n2e2YXjSnDGO56FO0thMAKwirVXob1me1Evg6pHXqKDTT7ey_D-zbFUNDTQYC7yg/exec'
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbznuAJHurx9OKlBxCyPJE1kwiExHlh1bFndIxH72owhgFHAo_sgHxEKQcLC8CXDR19GRA/exec'
 };
