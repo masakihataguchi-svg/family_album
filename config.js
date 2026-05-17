64d87a9f79ff859861c223f27c5376546001a1c1
masakihataguchi-svg <masaki.hataguchi@gmail.com>
2026-05-17 18:43:32 +0700

    動画対応

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  */
 const CONFIG = {
   // 本番用のGASウェブアプリURLを設定してください
-  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbxBoZ5aC5FhhDumVX7fZe_wJz5KWH6IXMoOHYzFnGxr_rBg4W0cK9gGKQy69ntFQarXXg/exec',
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbz2AumKKM2uZtP5punInUNvZLgEBf8oJiyseqNd6HxiOZKr1O8-4GKgiZrDgrDIy0xldA/exec',
   
   // 【移動】アルバムを開くための共通パスワード
   ALBUM_PASSWORD: 'Fuse1220-4'
