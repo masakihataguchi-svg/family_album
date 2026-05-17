3132a24a1dedf3b61346b3d3dba998017fb75485
masakihataguchi-svg <masaki.hataguchi@gmail.com>
2026-05-17 16:35:48 +0700

    セキュリティ対策

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
  */
 const CONFIG = {
   // 新しくデプロイしたGASのWebアプリURLをここに貼り付けてください
-  GAS_API_URL: 'https://script.google.com/macros/s/AKfycby1Mdj5nxuceyjsbLgeBuwuVctKVfU145e9XKjHRIZW2bCNyzYIvSCPZVB-VidZZ7O2Cg/exec'
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbzyv4bfO_RiTkzDUwM2KqSs0PdLnXE3QpSv9oEY792mpgcMU07FuH2SFYciGR2yCnXzoA/exec'
 };
