85141b0c8fa75f2165d8fb2ad5a65f3a9a447f6a
masakihataguchi-svg <masaki.hataguchi@gmail.com>
2026-05-17 14:51:03 +0700

    LINEからの自動アップロード対応

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
  */
 const CONFIG = {
   // 新しくデプロイしたGASのWebアプリURLをここに貼り付けてください
-  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbz0HNy8HJq5Ky1CFfCroUkADFtoGz6WyafB167RT8qUaiOfRFdmVQsjxdGr06Jni78w/exec'
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbzxncSJ7sysL9iiDpv6TaAB5J2kO-oAj7q2KpQL0nXvHYk8gtCqOZwfFW_1DMTD4uVkeA/exec'
 };
