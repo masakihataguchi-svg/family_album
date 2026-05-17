4a8a44a7511fa29360bc6039285f108313c006be
masakihataguchi-svg <masaki.hataguchi@gmail.com>
2026-05-17 14:59:13 +0700

    バグFix

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
  */
 const CONFIG = {
   // 新しくデプロイしたGASのWebアプリURLをここに貼り付けてください
-  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbzxncSJ7sysL9iiDpv6TaAB5J2kO-oAj7q2KpQL0nXvHYk8gtCqOZwfFW_1DMTD4uVkeA/exec'
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbzgv0n2e2YXjSnDGO56FO0thMAKwirVXob1me1Evg6pHXqKDTT7ey_D-zbFUNDTQYC7yg/exec'
 };
