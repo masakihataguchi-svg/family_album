38926b8641e02bfdb6706d8008b831d8ac73491e
masakihataguchi-svg <masaki.hataguchi@gmail.com>
2026-05-17 16:50:47 +0700

    セキュリティ再強化

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 /**
- * アプリケーション設定ファイル
+ * 家族アルバム - アプリケーション設定ファイル
  */
 const CONFIG = {
-  // 新しくデプロイしたGASのWebアプリURLをここに貼り付けてください
-  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbz7NKSFF8k3adFihzNtH3aqvNHEd0m1P7LrXsZ-lLsFq9-IWqQ4wjevCZomg8t2QkL5-A/exec'
+  // 本番用のGASウェブアプリURLを設定してください
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbz7NKSFF8k3adFihzNtH3aqvNHEd0m1P7LrXsZ-lLsFq9-IWqQ4wjevCZomg8t2QkL5-A/exec',
+  
+  // 【移動】アルバムを開くための共通パスワード
+  ALBUM_PASSWORD: 'Fuse1220-4'
 };
