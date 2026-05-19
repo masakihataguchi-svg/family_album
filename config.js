858fbff7146dbab73bf502c734d49199a3ae0540
masakihataguchi-svg <masaki.hataguchi@gmail.com>
2026-05-19 13:38:21 +0700

    チャットからの削除指令対応

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  */
 const CONFIG = {
   // 本番用のGASウェブアプリURLを設定してください
-  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbzx4duSP3VqJiEUykeUpzAdZY1ABHHAnWFgH4r9vLTRZvKe3ESCpH4XiQkIX1wNHXguzQ/exec',
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbyfldX3aytPp2K1hG68C9bwNH5QtFd7EYuSKqFltxiBR8d29WL2sIQ5AlpMzYWJNxok7A/exec',
   
   // 【移動】アルバムを開くための共通パスワード
   ALBUM_PASSWORD: 'Fuse1220-4'
