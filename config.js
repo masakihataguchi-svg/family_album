e01edce33865a6594364f49168e994818fe4ea00
masakihataguchi-svg <masaki.hataguchi@gmail.com>
2026-05-19 12:22:06 +0700

    画像・アルバム削除機能追加

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  */
 const CONFIG = {
   // 本番用のGASウェブアプリURLを設定してください
-  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbz-9y0ixobEBfv5jRUhnu8MFs2IPMoZ2UeeKd1qgIuVc9ORLqLmtBrB2prqh344GzcUWQ/exec',
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbyBzz-1tNFoNzlTmUmAWYH1S-pYt7MFdNe19pP0wCii_vuaQ6hlX6pGYVNJYZOe4pKtDA/exec',
   
   // 【移動】アルバムを開くための共通パスワード
   ALBUM_PASSWORD: 'Fuse1220-4'
